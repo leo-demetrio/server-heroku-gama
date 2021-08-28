4328f1501410469bf9d37c33d40cc6157c3a99ec
leo-demetrio <leocdemetrio@gmail.com>
2021-08-28 18:23:50 -0300

    add log user repository

--- src/repositories/userRepository.ts
+++ src/repositories/userRepository.ts
@@ -8,6 +8,7 @@ class UserRepository {
     }
     public async createUser(body) {
         try{
+            console.log("UserRepository +++++++++++++++++  " + body);
             const user = await this.userModel.create(body);
             return user;
         }catch(e){
